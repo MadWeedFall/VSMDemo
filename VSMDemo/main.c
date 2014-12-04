531c436c1cca2f1afbfa978d83ba901c9b0fe7ea
lixu1 <lixu1@mail.chukong-inc.com>
2014-12-04 11:34:25 +0800

    Initial Commit

---
+++ VSMDemo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VSMDemo
+//
+//  Created by CK03-007 on 14-12-4.
+//  Copyright (c) 2014年 CK03-007. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
